40b3871b6b07c30ded94c8a54775926cf93c65ca
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-07-25 17:00:43 +0800

    Setup app to display messages from ChatKit instance in real-time

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
 import React, { Component } from 'react';
+import Chatkit from '@pusher/chatkit'
 import MessageList from './components/MessageList'
-import SendMessageFrom from './components/SendMessageForm'
-import RoomList from './components/RoomList'
-import NewRoomForm from './components/NewRoomForm'
+// import SendMessageFrom from './components/SendMessageForm'
+// import RoomList from './components/RoomList'
+// import NewRoomForm from './components/NewRoomForm'
+
+import { tokenUrl, instanceLocator } from './config'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      messages: []
+    }
+  }
+
+  componentDidMount() {
+    const chatManager = new Chatkit.ChatManager({
+      instanceLocator,
+      userId: 'sysadmin',
+      tokenProvider: new Chatkit.TokenProvider({
+        url: tokenUrl
+      })
+    })
+
+    chatManager.connect()
+    .then(currentUser => {
+      currentUser.subscribeToRoom({
+        roomId: 12406667,
+        hooks: {
+          onNewMessage: message => {
+            this.setState({
+              messages: [...this.state.messages, message]
+            })
+          }
+        }
+      })
+    })
+  }
   render() {
     return (
       <div className="App">
-        <RoomList />
-        <MessageList />
-        <SendMessageFrom />
-        <NewRoomForm />
+        <MessageList messages={this.state.messages}/>
       </div>
     );
   }

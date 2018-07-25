b4ebbd9f3bc839c7b8be078a67f38365d16fb16e
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-07-25 15:26:36 +0800

    Initialize project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import MessageList from './components/MessageList'
+import SendMessageFrom from './components/SendMessageForm'
+import RoomList from './components/RoomList'
+import NewRoomForm from './components/NewRoomForm'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/config.js
@@ -0,0 +1,5 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/81620bab-06d9-4e5c-9aad-71d4cbec5264/token"
+const instanceLocator = "v1:us1:81620bab-06d9-4e5c-9aad-71d4cbec5264"
+
+exports.tokenUrl = tokenUrl
+exports.instanceLocator = instanceLocator

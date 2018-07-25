31aace0fd1a8e844f0f2bf328389e90c76ba1fac
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-07-25 17:24:31 +0800

    Add Message component
    Migrate message display from MessageList component to Message component
    
    Refactor Message component from class to function

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Message from './Message'
 
 class MessageList extends Component {
   render() {
@@ -7,10 +8,7 @@ class MessageList extends Component {
         {
           this.props.messages.map((message, index) => {
             return (
-              <div key={index} className="message">
-                <div className="message-username">{message.senderId}</div>
-                <div className="message-text">{message.text}</div> 
-              </div>
+              <Message key={index} username={message.senderId} text={message.text}/>
             )
           })
         }

ff5ca9c2fc05e94598fcf62ffb7481af40ce1005
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-07-26 19:12:00 +0800

    Add UX elements for user to join a chat room
    Enable message posting only when user joins a chat room

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -18,6 +18,16 @@ class MessageList extends Component {
   }
 
   render() {
+    if (!this.props.roomId) {
+      return (
+        <div className="message-list">
+          <div className="join-room">
+            &larr; Join a room!
+        </div>
+
+        </div>
+      )
+    }
     return (
       <div className="message-list">
         {

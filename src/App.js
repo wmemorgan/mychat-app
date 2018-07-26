da043273c41053de5a2c20eadb398a0b40f5d75c
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-07-26 12:47:07 +0800

    Add function to display a list of chat rooms

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Chatkit from '@pusher/chatkit'
 import MessageList from './components/MessageList'
 import SendMessageFrom from './components/SendMessageForm'
-// import RoomList from './components/RoomList'
+import RoomList from './components/RoomList'
 // import NewRoomForm from './components/NewRoomForm'
 
 import { tokenUrl, instanceLocator } from './config'
@@ -11,7 +11,9 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      messages: []
+      messages: [],
+      joinableRooms: [],
+      joinedRooms: []
     }
     this.sendMessage = this.sendMessage.bind(this)
   }
@@ -28,6 +30,16 @@ class App extends Component {
     chatManager.connect()
     .then(currentUser => {
       this.currentUser = currentUser
+
+      this.currentUser.getJoinableRooms()
+      .then(joinableRooms => {
+        this.setState({
+          joinableRooms,
+          joinedRooms: this.currentUser.rooms
+        })
+      })
+      .catch(err => console.log(`error on joinableRooms: ${err}`))
+
       this.currentUser.subscribeToRoom({
         roomId: 12406667,
         hooks: {
@@ -39,6 +51,7 @@ class App extends Component {
         }
       })
     })
+    .catch(err => console.log(`error on connecting: ${err}`))
   }
 
   sendMessage(text) {
@@ -51,6 +64,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <RoomList rooms={[...this.state.joinableRooms, ...this.state.joinedRooms]}/>
         <MessageList messages={this.state.messages}/>
         <SendMessageFrom sendMessage={this.sendMessage}/>
       </div>
